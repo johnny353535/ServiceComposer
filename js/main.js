524edcd78498eb77de4f08eeabd7d6d2c2a957d1
Jonas Hartweg <elmos-erbe@web.de>
2014-11-17 15:40:06 +0100

    Fetch data via Ajax from file

--- js/main.js
+++ js/main.js
@@ -23,109 +23,23 @@ require(["react", "components/FlowWrapper", "jquery", "html5shiv", "respond", "p
 
     React.initializeTouchEvents(true);
 
-    var data = {
-        test: "test",
-        flow: [
-            {
-                type: "activity", name: "test12", description: "description"
-            },
-            {
-                type: "fragment",
-                fragmentType: "conditional",
-                name: "conditional!",
-                condition: "x > 41",
-                description: "this is a description",
-                flows: [
-                    {
-                        name: "true",
-                        flow: [
-                            {type: "activity", name: "test3"},
-                            {type: "activity", name: "test4", description: "description1"}
-                        ]
-                    },
-                    {
-                        name: "false",
-                        flow: [
-                            {type: "activity", name: "test32"},
-                            {type: "activity", name: "test48"},
-                        ]
-                    }
-                ]
-            },
-            {
-                type: "fragment",
-                fragmentType: "parallel",
-                name: "parallel!",
-                description: "this is a description",
-                flows: [
-                    {
-                        name: "Hey",
-                        flow: [
-                            {type: "activity", name: "test3"},
-                            {type: "activity", name: "test4", description: "description1"}
-                        ]
-                    },
-                    {
-                        name: "You",
-                        flow: [
-                            {type: "activity", name: "test32"},
-                            {type: "activity", name: "test48"},
-                        ]
-                    },
-                    {
-                        name: "Me",
-                        flow: [
-                            {type: "activity", name: "test2"},
-                            {type: "activity", name: "test408"},
-                        ]
-                    }
-                ]
-            },
-            {
-                type: "fragment",
-                fragmentType: "loop",
-                name: "loop!",
-                description: "this is a description",
-                condition: "y < 3",
-                flows: [
-                    {
-                        name: "Hey",
-                        flow: [
-                            {type: "activity", name: "test3"},
-                            {type: "activity", name: "test4", description: "description1"}
-                        ]
-                    },
-                    {
-                        name: "You",
-                        flow: [
-                            {type: "activity", name: "test32"},
-                            {type: "activity", name: "test48"},
-                        ]
-                    },
-                    {
-                        name: "Me",
-                        flow: [
-                            {type: "activity", name: "test2"},
-                            {type: "activity", name: "test408"},
-                        ]
-                    }
-                ]
-            }
-        ]
-    }
+    var init = function(data) {
 
-    React.render(
-      <FlowWrapper data={data} />,
-      document.getElementById('contentWrapper')
-    );
+        React.render(
+          <FlowWrapper data={data} />,
+          document.getElementById('contentWrapper')
+        );
 
-    $('.addComponentWrapper').height($('body').height() - $('.flowWrapper:first-child li:first-child').offset().top);
+        $('.addComponentWrapper').height($('body').height() - $('.flowWrapper:first-child li:first-child').offset().top);
 
-	$('#addComponentButton').click(function(){
-		$(this).toggleClass('active');
-		$('.addComponentWrapper').toggleClass('active');
-		$('.contentWrapper').toggleClass('dim');
-		$('.flowWrapper').toggleClass('inactive');
-	});
+        $('#addComponentButton').click(function(){
+            $(this).toggleClass('active');
+            $('.addComponentWrapper').toggleClass('active');
+            $('.contentWrapper').toggleClass('dim');
+            $('.flowWrapper').toggleClass('inactive');
+        });
+    };
+
+    $.getJSON("data.json").done(init);
 
 });
