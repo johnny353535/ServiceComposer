02648d96aba6e1adbef4e10a2c62b7ed488b0a1b
Jonas Hartweg <elmos-erbe@web.de>
2015-01-23 15:26:30 +0100

    Server replies with BPMN

--- server/server.js
+++ server/server.js
@@ -9,14 +9,16 @@ app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
 
 app.post('/bpmn', function (req, res) {
 
-	console.log(req.body);
+  var json = req.body;
+
+	console.log(json);
 
 	res.header("Access-Control-Allow-Origin", "*");
-  	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
 
-  	res.set('Content-Type', 'text/xml');
-	res.send('<test>Hallo</test>');
+  res.set('Content-Type', 'text/xml');
+  res.send(JSONtoXML(json));
 })
 
 var server = app.listen(3000, function () {
@@ -114,30 +116,37 @@ var json = {
   ]
 }
 
-var flow = json.flow;
 //JSONtoXML(flow);
 
 function JSONtoXML(json){
 
+  var flow = json.flow;
 
-	var bpmn = "";
+	var components = "";
 	var sequence = "";
 
-	bpmn += '<startEvent id="startevent1" name="Start"></startEvent>\n';
+	components += '<startEvent id="startevent1" name="Start"></startEvent>\n';
 	sequence += '<sequenceFlow id="flow1" sourceRef="startevent" targetRef="'+flow[0].uid+'"></sequenceFlow>\n';
 
 	for(var i = 0; i < flow.length; i++){
 		var elem = flow[i];
 		var nextElem = flow[i + 1];
 
-		bpmn += "";
+		components += "";
 		if(i < flow.length - 1) sequence += '<sequenceFlow id=\"flow'+(i+2)+'\" sourceRef=\"'+elem.uid+'\" targetRef=\"'+nextElem.uid+'\"></sequenceFlow>\n'
 
 	}
 
-	bpmn += '<endEvent id="endevent1" name="End"></endEvent>';
-	sequence += '<sequenceFlow id="flow'+(flow.length + 1)+'" sourceRef="'+flow[flow.length - 1].uid+'" targetRef="endevent1"></sequenceFlow>';
+	components += '<endEvent id="endevent1" name="End"></endEvent>';
+	sequence += '<sequenceFlow id="flow'+(flow.length + 1)+'" sourceRef="'+flow[flow.length - 1].uid+'" targetRef="endevent1"></sequenceFlow>\n';
 
 	//console.log(sequence)
+
+  var bpmn = (components + '\n\n' + sequence);
+
+  console.log('JSON:', json);
+  console.log('BPMN:', bpmn);
+
+  return bpmn;
 
 }
