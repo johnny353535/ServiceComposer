c0b3d2755401f077905588ccb3511869a5a88e6e
Jonas Hartweg <elmos-erbe@web.de>
2015-01-28 15:12:12 +0100

    Get activities and fragments from server

--- server/server.js
+++ server/server.js
@@ -1,3 +1,6 @@
+var activities = require('../data/activities.json');
+var fragments = require('../data/fragments.json');
+
 var express = require('express');
 var app = express();
 
@@ -21,6 +24,31 @@ app.post('/bpmn', function (req, res) {
   res.send(JSONtoXML(json));
 })
 
+// Deliver activities
+app.get('/activities', function(req, res) {
+
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+
+  res.set('Content-Type', 'text/json');
+  res.send(activities);
+
+})
+
+// Deliver fragments
+app.get('/fragments', function(req, res) {
+
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+
+  res.set('Content-Type', 'text/json');
+  res.send(fragments);
+
+})
+
+
 var server = app.listen(3000, function () {
 
   var host = server.address().address
@@ -29,92 +57,6 @@ var server = app.listen(3000, function () {
   console.log('Server listening at http://%s:%s', host, port)
 
 })
-
-
-
-var json = {
-  "uid": 1421159066349,
-  "name": "MyFlow",
-  "type": "flow",
-  "glyphicon": "glyphicon-asterisk",
-  "description": "kein text",
-  "flow": [
-    {
-      "id": "aj9d0ajsd09a",
-      "name": "Light",
-      "description": "Control the light",
-      "glyphicon": "glyphicon-certificate",
-      "url": "",
-      "type": "activity",
-      "uid": 1421159069803
-    },
-    {
-      "id": "akspoda0s9d2",
-      "name": "Door",
-      "description": "Lock and unlock the door",
-      "glyphicon": "glyphicon-home",
-      "url": "",
-      "type": "activity",
-      "uid": 1421159072351
-    },
-    {
-      "id": "weather",
-      "type": "fragment",
-      "fragmentType": "conditional",
-      "name": "Weather",
-      "description": "Reacts based on the weather condition",
-      "glyphicon": "glyphicon-cloud",
-      "sentence": "If the weather is",
-      "options": [
-        {
-          "name": "sunny",
-          "glyphicon": "glyphicon-certificate"
-        },
-        {
-          "name": "cloudy",
-          "glyphicon": "glyphicon-cloud"
-        },
-        {
-          "name": "rainy",
-          "glyphicon": "glyphicon-tint"
-        },
-        {
-          "name": "windy",
-          "glyphicon": "glyphicon-flag"
-        }
-      ],
-      "flows": [
-        {
-          "uid": 1421159076741,
-          "name": "sunny",
-          "type": "flow",
-          "glyphicon": "glyphicon-certificate",
-          "description": "kein text",
-          "flow": [
-            {
-              "id": "as0kd9a9d",
-              "name": "Play music",
-              "description": "Play a song or playlist",
-              "glyphicon": "glyphicon-music",
-              "url": "",
-              "type": "activity",
-              "uid": 1421159082233
-            }
-          ]
-        },
-        {
-          "uid": 1421171683601,
-          "name": "rainy",
-          "type": "flow",
-          "glyphicon": "glyphicon-tint",
-          "description": "kein text",
-          "flow": []
-        }
-      ],
-      "uid": 1421159074938
-    }
-  ]
-}
 
 //JSONtoXML(flow);
 
