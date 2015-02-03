5ed7f02a211c64ca3afefb1bac36b33fb20ee866
Jonas Hartweg <elmos-erbe@web.de>
2015-02-03 17:06:22 +0100

    tutorial text

--- js/components/Tutorial.react.js
+++ js/components/Tutorial.react.js
@@ -6,11 +6,14 @@ define(["react"], function(React) {
 			return(
 				<div className="tutorial">
 					<div className="help">
-						<h4>Help</h4>
+						<h4>Getting started</h4>
 						<ul>
-							<li>Tap <span className="glyphicon glyphicon-book"></span> to load a composition</li>
+							<li>Tap <span className="glyphicon glyphicon-book"></span> to load a composition - or -</li>
 							<li>Tap <span className="glyphicon glyphicon-plus-sign"></span> to create a new composition</li>
 						</ul>
+						<h4>Help</h4>
+						<p>A service composition defines a sequence of operations that shall be automized.</p>
+						<p>There are three times of elements in a service composition: activities, fragments and compositions.</p>
 					</div>
 				</div>
 			)
