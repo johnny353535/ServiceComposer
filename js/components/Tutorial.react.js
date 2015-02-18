917c9a77bd894846a6e488c8f4aa3c0f993200b7
Jonas Hartweg <elmos-erbe@web.de>
2015-02-18 19:12:07 +0100

    Changes to the tutorial

--- js/components/Tutorial.react.js
+++ js/components/Tutorial.react.js
@@ -10,13 +10,32 @@ define(["react"], function(React) {
 					<div className="help">
 						<h4>Getting started</h4>
 						<ul>
-							<li>Tap <span className="glyphicon glyphicon-book"></span> to load a composition - or -</li>
+							<li>Tap <span className="glyphicon glyphicon-book"></span> to load a composition</li>
 							<li>Tap <span className="glyphicon glyphicon-plus-sign"></span> to create a new composition</li>
 						</ul>
-						<h4>Help</h4>
+
+						{/*
+						<h4>Terminology</h4>
 						<p>A service composition defines a sequence of operations that shall be automized.</p>
 						<p>There are three times of elements in a service composition: activities, fragments and compositions.</p>
 						<p>Tap <span className="glyphicon glyphicon-question-sign"></span> to reopen this help any time.</p>
+
+						<h4>Adding Elements to your Service Composition</h4>
+						<p>Text</p>
+
+						<h4>Configuring Activities</h4>
+						<p>Text</p>
+
+						<h4>Configuring Fragments</h4>
+						<p>Text</p>
+
+						<h4>Deleting Elements</h4>
+						<p>Text</p>
+
+						<h4>Publishing your Service Composition</h4>
+						<p>Text</p>
+						*/}
+
 					</div>
 				</div>
 			)
