3650dc7f1b25949d80cc6f69520847cfabe401cf
Jonas Hartweg <elmos-erbe@web.de>
2015-03-01 18:54:28 +0100

    Recursively search JSON for BPMN

--- server/server.js
+++ server/server.js
@@ -62,31 +62,98 @@ function JSONtoXML(json){
 
   var flow = json.flow;
 
-	var components = "";
+  var xml = '<?xml version="1.0" encoding="UTF-8"?>\n';
+  var definitions = '<definitions xmlns="http://www.omg.org/spec/BPMN/20100524/MODEL" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:activiti="http://activiti.org/bpmn" xmlns:bpmndi="http://www.omg.org/spec/BPMN/20100524/DI" xmlns:omgdc="http://www.omg.org/spec/DD/20100524/DC" xmlns:omgdi="http://www.omg.org/spec/DD/20100524/DI" typeLanguage="http://www.w3.org/2001/XMLSchema" expressionLanguage="http://www.w3.org/1999/XPath" targetNamespace="http://www.activiti.org/test">\n';
+  var process = '<process id="'+json.uid+'" name="'+json.name+'" isExecutable="true">\n';
+
+	var elements = "";
 	var sequence = "";
 
-	components += '<startEvent id="startevent1" name="Start"></startEvent>\n';
-	sequence += '<sequenceFlow id="flow1" sourceRef="startevent" targetRef="'+flow[0].uid+'"></sequenceFlow>\n';
+	elements += '<startEvent id="startevent1" name="Start"></startEvent>\n';
+  elements += collectElements(flow);
+	elements += '<endEvent id="endevent1" name="End"></endEvent>\n';
 
-	for(var i = 0; i < flow.length; i++){
-		var elem = flow[i];
-		var nextElem = flow[i + 1];
-
-		components += "";
-		if(i < flow.length - 1) sequence += '<sequenceFlow id=\"flow'+(i+2)+'\" sourceRef=\"'+elem.uid+'\" targetRef=\"'+nextElem.uid+'\"></sequenceFlow>\n'
-
-	}
-
-	components += '<endEvent id="endevent1" name="End"></endEvent>';
+  sequence += '<sequenceFlow id="flow1" sourceRef="startevent" targetRef="'+flow[0].uid+'"></sequenceFlow>\n';
+  sequence += exploreFlowSequence(flow);
 	sequence += '<sequenceFlow id="flow'+(flow.length + 1)+'" sourceRef="'+flow[flow.length - 1].uid+'" targetRef="endevent1"></sequenceFlow>\n';
 
 	//console.log(sequence)
 
-  var bpmn = (components + '\n\n' + sequence);
+  // Build the BPMN
 
-  console.log('JSON:', json);
-  console.log('BPMN:', bpmn);
+  var bpmn =
+        xml + '\n'+
+        definitions +
+        process + '\n' +
+        elements + '\n' +
+        sequence + '\n' +
+        '</process>\n' +
+        '</definitions>';
+
+  //console.log('JSON:', json);
+  //console.log('BPMN:', bpmn);
 
   return bpmn;
 
+}
+
+// Recursive function that searches for elements in flows and subflows
+function collectElements(flow){
+  var elements = '';
+
+  // Collect elements
+  for(var i = 0; i < flow.length; i++){
+
+    var elem = flow[i];
+
+    // Append element
+    switch(elem.type){
+      case "activity":
+        elements += '<serviceTask id="'+elem.id+'" name="'+elem.name+'"></serviceTask>\n';
+        break;
+      case "fragment":
+        elements += '<exclusiveGateway id="'+elem.id+'" name="'+elem.name+'"></exclusiveGateway>\n';
+        break;
+    }
+
+    // Any subflows?
+    if(flow[i].flows){
+      var flows = flow[i].flows;
+      // recursively call collectElements on subflows
+      for(var j = 0; j < flows.length; j++){
+        var subflow = flow[i].flows[j].flow;
+        elements += collectElements(subflow)
+      }
+    }
+  }
+
+  return elements;
+}
+
+function exploreFlowSequence(flow){
+  var sequence = '';
+
+  // Iterate through flow
+	for(var i = 0; i < flow.length; i++){
+
+		var elem = flow[i];
+		var nextElem = flow[i + 1];
+
+    // Write sequence
+		if(nextElem){
+      sequence += '<sequenceFlow id=\"flow'+(i+2)+'\" sourceRef=\"'+elem.uid+'\" targetRef=\"'+nextElem.uid+'\"></sequenceFlow>\n'
+    }
+
+    // Any subflows?
+    if(flow[i].flows){
+      var flows = flow[i].flows;
+      // recursively call exploreFlowSequence on subflows
+      for(var j = 0; j < flows.length; j++){
+        var subflow = flow[i].flows[j].flow;
+        sequence += exploreFlowSequence(subflow)
+      }
+    }
+	}
+
+  return sequence
 }
